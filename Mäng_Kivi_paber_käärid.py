9f9ee80378e2c24eba4cb056d84da543d3928ba7
opilane <opilane@HP-CZC2349HT9>
2025-03-12 10:15:12 +0200

    .

--- Mäng_Kivi_paber_käärid.py
+++ Mäng_Kivi_paber_käärid.py
@@ -1,15 +1,2 @@
-import random
-
-print("Tere tulemast m‰ngule 'Kivi-paber-k‰‰rid'.")
-
-choices = ["Kivi", "Paber", "K‰‰rid"]
-
-rules = [["Tasav‰gise", "Arvuti vıidab!", "Sa vıidad!"],
-         ["Sa vıidad!", "Tasav‰gise", "Arvuti vıidab!"],
-         ["Arvuti vıidab!", "Sa vıidad!", "Tasav‰gise"]]
-
-m‰ngija_choice = input("Vali (Kivi, paber, k‰‰rid): ").capitilize()
-if m‰ngija_choice not in choices:
-    print("Vale sisestus, proovi uuesti.")
-else:
-    
+print("Hello world!")
+print ("Idiot!")
